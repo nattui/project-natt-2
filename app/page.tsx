f8d537a6db2e21385afcfe405cc6e46dec289280
Natt Nguyen <natt.lefevre@gmail.com>
2025-12-25 14:23:32 -0800

    style: update Home component colors and text properties for improved design consistency

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       {/* Topbar */}
-      <div className="d fixed top-0 right-0 left-0 z-10 h-64 bg-primary-1">topbar</div>
+      <div className="d fixed top-0 right-0 left-0 z-10 h-64 bg-gray-2">topbar</div>
 
       {/* Section 1 */}
       <section className="h-[1200px]">
@@ -15,7 +15,7 @@ export default function Home() {
           Meets Design
         </h1>
         <Spacer className="h-16" />
-        <p className="mx-auto max-w-512 text-center text-[22px]">
+        <p className="mx-auto max-w-512 hyphens-none text-center text-[22px]">
           Design engineer crafting refined applications, interfaces, and experiences. Dreamer and
           doer.
         </p>
@@ -23,7 +23,7 @@ export default function Home() {
 
       {/* Section 2 */}
       <section className="d h-[2200px]">
-        <div className="d sticky top-64 h-dvh w-[200px] bg-primary-1">cat</div>
+        <div className="d sticky top-64 h-dvh w-[200px] bg-gray-2">cat</div>
       </section>
 
       {/* Section 3 */}
