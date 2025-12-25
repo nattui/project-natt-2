4e9b228b3aa772a8d30b96818f99dbbb62ebfdb8
Natt Nguyen <natt.lefevre@gmail.com>
2025-12-25 14:20:05 -0800

    feat: enhance Home component with new content and spacing using Nattui's Spacer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Spacer } from "@nattui/react-components"
+
 export default function Home() {
   return (
     <div className="flex flex-col">
@@ -5,7 +7,19 @@ export default function Home() {
       <div className="d fixed top-0 right-0 left-0 z-10 h-64 bg-primary-1">topbar</div>
 
       {/* Section 1 */}
-      <section className="h-[1200px] pt-64">splash</section>
+      <section className="h-[1200px]">
+        <Spacer className="h-192" />
+        <h1 className="text-center text-72 leading-[1.1]">
+          Software
+          <br />
+          Meets Design
+        </h1>
+        <Spacer className="h-16" />
+        <p className="mx-auto max-w-512 text-center text-[22px]">
+          Design engineer crafting refined applications, interfaces, and experiences. Dreamer and
+          doer.
+        </p>
+      </section>
 
       {/* Section 2 */}
       <section className="d h-[2200px]">
