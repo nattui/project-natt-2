b54c8e39bdd7a269ef340aae0d2a53e94c7eaf30
Natt Nguyen <natt.lefevre@gmail.com>
2025-12-25 15:17:55 -0800

    feat: update BlogPage to display a list of blog posts with links, removing unnecessary sections and components

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,51 +1,22 @@
-"use client"
-
-import { IconNotebook } from "@nattui/icons-line"
-import { Button, Spacer } from "@nattui/react-components"
-import { useRouter } from "next/navigation"
+import Link from "next/link"
 
 export default function BlogPage() {
-  const router = useRouter()
-
-  function onRoute() {
-    router.push("/")
-  }
+  // I want to show all the blog posts in a list
 
   return (
     <div className="flex flex-col">
-      {/* Topbar */}
-      <div className="d fixed top-0 right-0 left-0 z-10 h-64 bg-gray-2">topbar</div>
-
-      {/* Section 1 */}
-      <section className="flex h-[1200px] flex-col px-16">
-        <Spacer className="h-192" />
-        <h1 className="text-center text-72 leading-[1.1] max-768:max-w-320 max-768:text-48">
-          Blog
-        </h1>
-        <Spacer className="h-16" />
-        <p className="mx-auto max-w-512 hyphens-none text-center text-[22px] max-768:text-18">
-          Design engineer crafting refined applications, interfaces, and experiences. Dreamer and
-          doer.
-        </p>
-        <Spacer className="h-24" />
-        <div className="mx-auto flex items-center justify-center">
-          <Button
-            iconEnd={<IconNotebook size={16} />}
-            onClick={onRoute}
-            variant="secondary"
-          >
-            Home
-          </Button>
-        </div>
-      </section>
-
-      {/* Section 2 */}
-      <section className="d h-[2200px]">
-        <div className="d sticky top-64 h-dvh w-[200px] bg-gray-2">cat</div>
-      </section>
-
-      {/* Section 3 */}
-      <section className="h-[600px]">footer</section>
+      <Link
+        className="w-fit hover:underline"
+        href="/blog/2025-12-25-hello"
+      >
+        Hello
+      </Link>
+      <Link
+        className="w-fit hover:underline"
+        href="/blog/2025-12-25-world"
+      >
+        World
+      </Link>
     </div>
   )
 }
