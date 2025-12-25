8c0c93587262a43587e0f53dd1c7af5f1c8f68b0
Natt Nguyen <natt.lefevre@gmail.com>
2025-12-25 14:06:49 -0800

    feat: refactor RootLayout to utilize new font utilities and improve body styling with Tailwind CSS

---
+++ utils/fonts.ts
@@ -0,0 +1,26 @@
+import { Geist_Mono as fontMono, Inter as fontSans } from "next/font/google"
+import localFont from "next/font/local"
+
+// Martina Plantijn 300
+const display = localFont({
+  src: [{ path: "./font-display-300.woff2", weight: "500" }],
+  variable: "--font-display",
+})
+
+const mono = fontMono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+  weight: ["400", "500"],
+})
+
+const sans = fontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  weight: ["400", "500"],
+})
+
+export const fontCssVariables = [
+  display.variable,
+  mono.variable,
+  sans.variable,
+].join(" ")
