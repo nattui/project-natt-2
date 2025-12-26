bb79bc811b45954f1ea98d5d17c730ffe610a933
Natt Nguyen <natt.lefevre@gmail.com>
2025-12-25 19:37:30 -0800

    refactor: change Topbar div to header and add semantic navigation elements for improved accessibility

--- components/topbar.tsx
+++ components/topbar.tsx
@@ -4,8 +4,9 @@ import { Logotype } from "@/components/logotype"
 
 export function Topbar() {
   return (
-    <div className="fixed top-0 right-0 left-0 z-10 flex h-64 items-center border-gray-6 border-b bg-gray-2 px-16">
+    <header className="fixed top-0 right-0 left-0 z-10 flex h-64 items-center border-gray-6 border-b bg-gray-2 px-16">
       <div className="mx-auto flex w-full max-w-768 items-center justify-between">
+        {/* Brand */}
         <Link
           className="-ml-8 flex w-fit items-center justify-center gap-x-8 p-8 transition-opacity hover:opacity-50"
           href="/"
@@ -14,7 +15,8 @@ export function Topbar() {
           <Logotype />
         </Link>
 
-        <div className="flex items-center gap-x-12">
+        {/* Navigation */}
+        <nav className="flex items-center gap-x-12">
           <Link
             className="font-500 text-14 hover:underline"
             href="/blog"
@@ -27,8 +29,8 @@ export function Topbar() {
           >
             Checklist
           </Link>
-        </div>
+        </nav>
       </div>
-    </div>
+    </header>
   )
 }
