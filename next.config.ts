cd7a5f58111a06c22aa781d4ce91eb08aaf30e7d
Natt Nguyen <natt.lefevre@gmail.com>
2025-12-25 16:02:39 -0800

    chore: simplify pageExtensions in next.config.ts to only include mdx and tsx

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import createMDX from "@next/mdx"
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  pageExtensions: ["mdx", "tsx"],
   reactCompiler: true,
   reactStrictMode: false,
   typedRoutes: true,
